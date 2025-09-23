81dfe64904ea8ab35b303e85eb4bfdd92113f4f5
Emma Keaveney <142708620+Emma-keaveney@users.noreply.github.com>
2025-09-23 16:13:07 +0100

    Galaxy collision with a point-mass perturber

--- simple_perturber.cpp
+++ simple_perturber.cpp
@@ -27,16 +27,16 @@ const float pi = 3.14159265358979323846;   //pi
  * @return the acceleration of the star in either the x or y direction
  */
 float accel(float x, float y,float x_c,float y_c, float x_p, float y_p, float M, float m_p, float e, bool x_dir){
-    float D_C = sqrt(pow(x,2) + pow(y,2)+ pow(e,2));
+    float D_C = sqrt(pow(x-x_c,2) + pow(y-y_c,2) + pow(e,2));
     float D_P = sqrt(pow(x-x_p,2) + pow(y-y_p,2) + pow(e,2));
 
     float acceleration;
 
     if (x_dir == true){
-        acceleration = -G*M*x/pow(D_C,3) - G*m_p*(x-x_p)/pow(D_P,3);
+        acceleration = -G*M*(x-x_c)/pow(D_C,3) - G*m_p*(x-x_p)/pow(D_P,3);
     }
     else{
-        acceleration = -G*M*y/pow(D_C,3) - G*m_p*(y-y_p)/pow(D_P,3);
+        acceleration = -G*M*(y-y_c)/pow(D_C,3) - G*m_p*(y-y_p)/pow(D_P,3);
     }
     return acceleration;
 }
@@ -79,7 +79,7 @@ std::vector<float> taylor_one_step(float x,float y, float x_c, float y_c, float 
 
 /**
  * 
- * @brief Uses the leapfrog method to find the new position and speed of a star at each time-step
+ * @brief Uses the leapfrog method of integration to find the new position and speed of a star at each time-step
  * 
  * @param x: x coordinate of the star
  * @param y: y coordinate of the star
@@ -118,22 +118,22 @@ std::vector<float> leapfrog_one_step(float x,float y, float x_c, float y_c, floa
 
 /**
  * 
- * @brief At each time-step calculates the new coordinates and velocities of each star using the function 
- * taylor_one_step, then recalculates the position of the barycentre of the galaxy. Assumes the perturber 
- * moves linearly, feeling no force from the galaxy, and the galaxy is initially stationary in the 
- * reference frame.
+ * @brief At each time-step calculates the new coordinates and velocities of each star using either the taylor expansion 
+ * or leapfrog method, then recalculates the position of the barycentre of the galaxy. Assumes the perturber moves diagonally, 
+ * feeling no force from the galaxy, and the galaxy is initially stationary.
  * 
  * @param x: starting x coordinates of the stars
  * @param y: starting y coordinates of the stars
  * @param v_x: starting x components of the stars's velocities
  * @param v_y: starting y components of the stars's velocities
- * @param v_p: velocity of the perturber
+ * @param v_p: velocity of the perturber in both the x and y direction
  * @param m: masses of the stars
  * @param m_p: the mass of the point perturber
  * @param a: time-step
  * @param e: the softening constant
  * @param n: the number of time-steps
  * @param initial: starting [x,y] position of the point perturber
+ * @param leapfrog: if true uses the leapfrog method, else uses the Taylor expansion method
  * 
  * @return an tuple containing the positions of each star at each time-step, the position of the perturber
  * at each time-step, and the position of the barycentre of the galaxy at each time-step.
@@ -141,7 +141,7 @@ std::vector<float> leapfrog_one_step(float x,float y, float x_c, float y_c, floa
 std::tuple< 
 std::vector<std::vector<std::vector<float>>>, std::vector<std::vector<float>>, std::vector<std::vector<float>>
 > 
-taylor_all_stars(std::vector<float> x,std::vector<float> y, std::vector<float> v_x, std::vector<float> v_y, float v_p, std::vector<float> m, float m_p, float a, float e, int n, std::vector<float> initial, bool leapfrog){
+all_stars(std::vector<float> x,std::vector<float> y, std::vector<float> v_x, std::vector<float> v_y, float v_p, std::vector<float> m, float m_p, float a, float e, int n, std::vector<float> initial, bool leapfrog){
    
     int num_stars = x.size();
     //to store the data we create an array, with a row for each time step so [time step][stars][x and y].
@@ -197,20 +197,20 @@ taylor_all_stars(std::vector<float> x,std::vector<float> y, std::vector<float> v
  * 
  * @param start: starting value of the vector
  * @param end: end value of the vector
- * @param n: number of values in the vector
+ * @param q: number of values in the vector
  * 
  * @return the vector result
  * 
  */
-std::vector<float> linspace(float start, float end, int n) {
+std::vector<float> linspace(float start, float end, int q) {
     std::vector<float> result;
-    if (n == 0) return result;
-    if (n == 1) {
+    if (q == 0) return result;
+    if (q == 1) {
         result.push_back(start);
         return result;
     }
-    float step = (end - start) / (n - 1);
-    for (int i = 0; i < n; i++) {
+    float step = (end - start) / (q - 1);
+    for (int i = 0; i < q; i++) {
         result.push_back(start + step * i);
     }
     return result;
@@ -240,34 +240,35 @@ std::vector<float> random_number(float start, float end, int q){
 /**
  * @brief Given certain parameters of a galaxy, returns the initial conditions of the stars.
  * 
- * This is done by modelling the initial positions of the stars uniformly between 0.1 and R, the 
- * radius of the galaxy. The masses are randomly distributed, and the initial circular velocities found 
- * using v = (GM/r)^(1/2) where M is the total mass of the galaxy. From the circular velocities of the 
- * stars, the orbital period can be found. The softening constant e = R_max p^(-1/2) is calculated.
-
  * @param p: number of stars
- * @param r_max: radius of the galaxy in kiloparsecs
- * @param m_min: minimum stellar mass of a star in the galaxy in solar masses
- * @param m_max: maximum stellar mass of a star in the galaxy in solar masses
+ * @param r_max: radius of the galaxy
  * 
  * @return a tuple containing the initial x and y coordinates of the stars, the x and y components of the
- * initial velocities of the stars, the masses of the stars, the total mass of the galaxy the softening 
- * constant, the shortest andlongest orbital periods of stars in the galaxy, and the speed of the slowest
+ * initial velocities of the stars, the masses of the stars, the total mass of the galaxy, the softening 
+ * constant, the shortest and longest orbital periods of stars in the galaxy, and the speed of the slowest
  * orbiting star in the galaxy
  */
 std::tuple<
 std::vector<float>, std::vector<float>, std::vector<float>, std::vector<float>, std::vector<float>, float, float, float, float, float
 >
-initialise_values(int p, float r_max, float m_min, float m_max){
+initialise_values(int p, float r_max){
 
-    std::vector<float> r = linspace(0.1, r_max, p);
+    // distribute stars according to the exponential distribution
+    float R_d = r_max/3;    //set scale length of the galaxy to be a third of the radius
+    std::vector<float> u = random_number(0,1,p);
+
+    std::vector<float> r;  //find circular velocities of the stars
+    for(int i = 0; i<p; i++){
+        r.push_back(-R_d*std::log(1-u[i]*(1-std::exp(-r_max/R_d))));
+    }
+
     std::vector<float> theta = random_number(0, 2*pi, p);
-    std::vector<float> m = random_number(m_min, m_max, p);
+    std::vector<float> m(p,1);
 
     float r_max_val = *std::max_element(r.begin(), r.end());
     float r_min_val = *std::min_element(r.begin(), r.end());
 
-    float e = r_max_val/sqrt(p);    //define the softening constant
+    float e = r_max/sqrt(p);    //define the softening constant
     std::cout<< "Softening constant is " << e <<std::endl;
 
     float M = std::accumulate(m.begin(), m.end(), 0.0f);   //total mass of the galaxy
@@ -294,8 +295,8 @@ initialise_values(int p, float r_max, float m_min, float m_max){
     for(int i = 0; i<r.size(); i++){
         x.push_back(r[i]*cos(theta[i]));
         y.push_back(r[i]*sin(theta[i]));
-        v_x.push_back(-v[i]*sin(theta[i]));
-        v_y.push_back(v[i]*cos(theta[i]));
+        v_x.push_back(v[i]*sin(theta[i]));
+        v_y.push_back(-v[i]*cos(theta[i]));
     }
 
     return std::make_tuple(x,y,v_x, v_y, m, M, e, T_min, T_max, v_min_val);
@@ -307,29 +308,27 @@ int main(){
     the galaxy. The initial position of the perturber can also be altered the change the distance of
     closest approach */
 
-    int p = 200;    //number of stars
-    float mu = 2;    //mass ratio of perturber to galaxy
-    float V = 0.8;   //ratio of perturber velocity to slowest star's circular velocity
+    int p = 1000;    //number of stars
+    float mu = 5;    //mass ratio of perturber to galaxy
+    float V = 1.2;   //ratio of perturber velocity to slowest star's circular velocity
     float r_max = 5;    //radius of galaxy
-    float m_min = 0.1;    // mass of smallest star in galaxy 
-    float m_max = 5;     // mass of largest star in galaxy
-    int n = 70000;    //number of time steps
-    std::vector<float> initial = {-20, 0};
+    int n = 50000;    //number of time steps
+    std::vector<float> initial = {-20, 10};
 
-    auto values = initialise_values(p, r_max, m_min, m_max);
+    auto values = initialise_values(p, r_max);
 
     float v_p = V*std::get<9>(values);
     std::cout << "Perturber speed is " << v_p << std::endl;
 
-    // set the time period of the simulation to be twice the time taken for the perturber to reach the galaxy 
-    float T = 2*sqrt(pow(initial[0],2)+pow(initial[1],2))/v_p;
+    // set the time period of the simulation to be triple the time taken for the perturber to reach the galaxy 
+    float T = 3*sqrt(pow(initial[0],2)+pow(initial[1],2))/v_p;
     std::cout << "Time scale of simulation is " << T << std::endl;
 
     float m_p = mu*std::get<5>(values);
     float a = T/n;
     std::cout << "Time-step is " << a <<std::endl;
 
-    auto results = taylor_all_stars(std::get<0>(values),std::get<1>(values), std::get<2>(values), std::get<3>(values), v_p, std::get<4>(values), m_p, a, std::get<6>(values), n, initial, true);
+    auto results = all_stars(std::get<0>(values),std::get<1>(values), std::get<2>(values), std::get<3>(values), v_p, std::get<4>(values), m_p, a, std::get<6>(values), n, initial, true);
     
     std::cout<< "Simulation run, saving data now" << std::endl;
 
@@ -338,25 +337,25 @@ int main(){
     std::vector<std::vector<float>> barycentre = std::get<2>(results); 
 
     //save the stars, perturber, and barycentre data
-    std::ofstream out("stars.txt");
+    std::ofstream out("simp_ex2_stars.txt");
     for (int i = 0; i < stars.size(); i++){
         for (int j =0; j < stars[0].size(); j++){
             out << stars[i][j][0] << "\t" << stars[i][j][1] << "\n";
         }
     }
 
-    std::ofstream out1("perturber.txt");
+    std::ofstream out1("simp_ex2_perturber.txt");
     for (int i = 0; i < perturber.size(); ++i) {
         out1 << perturber[i][0] << "\t" << perturber[i][1] << "\n";
     }
 
-    std::ofstream out2("barycentre.txt");
+    std::ofstream out2("simp_ex2_barycentre.txt");
     for (int i = 0; i < barycentre.size(); ++i) {
         out2 << barycentre[i][0] << "\t" << barycentre[i][1] << "\n";
     }
 
     //save the data containing the parameters
-    std::ofstream out3("parameters.txt");
+    std::ofstream out3("simp_ex2_parameters.txt");
         out3 << p << "\t" << mu << "\t" << V << "\t" << n ;
 
     out.close();
